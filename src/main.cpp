6e58a8b6fafd3d710e61ca2797fb9aca7575dfcb
LyfeLike <57576791+LyfeLike@users.noreply.github.com>
2024-05-20 12:19:53 +0200

    passes. code has good structure. very rustlike xD

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,73 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <stdexcept>
 
 enum class Move {Rock, Paper, Scissors};
 
 struct Round{
-    Move ours;
     Move theirs;
+    Move ours;
 };
+
+Move move_matcher(char input){
+    Move matched_move;
+    if (input == 'A'|| input == 'X') {
+        matched_move = Move::Rock;  
+        std::cout<< input << "= rock" << std::endl;  
+    } else if (input == 'B'|| input == 'Y') {
+        matched_move = Move::Paper;    
+        std::cout<< input << "= paper" << std::endl;  
+    } else if (input == 'C' || input == 'Z') {
+        matched_move = Move::Scissors;
+        std::cout<< input <<"= Scissors" << std::endl;     
+    } else {
+        std::cout<< "invalid input." << std::endl;
+        throw std::runtime_error("Unexpected case occurred");
+    }
+    return matched_move;
+}
+
+Round round_matcher(char left, char right){
+    Move thrown_move_left = move_matcher(left);
+    Move thrown_move_right = move_matcher(right);
+    Round matched_round = { thrown_move_left, thrown_move_right};
+    std::cout<< "returning a matched round\n";
+    return matched_round;
+}
+
+int round_resolution(Round round){
+    int score = 0;//this cannot be local
+    if (round.theirs == Move::Rock && round.ours == Move::Paper){
+        score += 8;
+    }else if (round.theirs == Move::Paper && round.ours == Move::Rock) {
+        score += 1;
+    }else if (round.theirs == Move::Scissors && round.theirs == round.ours){
+        score += 6;
+    }else{
+        std::cout<< "sad boom :(\n";
+    }
+    return score;
+}
 
 int main() {
     std::ifstream file("input.txt");
     std::stringstream buffer;
-
+    int final_score = 0;
     if (file.is_open()) {
-        buffer << file.rdbuf();
+        std::string line;
+        while (std::getline(file, line)){
+            std::istringstream iss(line);
+            char a, b;
+            if(!(iss >> a >> b)) { break; }//error
+            Round round = round_matcher(a,b);
+            final_score += round_resolution(round);
+        }
         file.close();
-
-        std::string contents = buffer.str();
-        std::cout<< "File contents:\n" << contents << std::endl;
     } else {
         std::cout << "Unable to open file :(" << std::endl;
     }
     //you need to iterate over contents and after that match the move, save that as ours or theirs, and calculate the outcoom;
-
+    std::cout<< "Final score= " << final_score <<std::endl;
     return 0;
 }
